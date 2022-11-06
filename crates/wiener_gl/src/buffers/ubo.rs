138b5c9bc9ed8092640084091a900dd6baaee87d
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-06 18:03:21 -0300

    Implemented UBOs

---
+++ crates/wiener_gl/src/buffers/ubo.rs
@@ -0,0 +1,81 @@
+use super::buffer::Buffer;
+use std::mem::size_of;
+
+use gl;
+use gl::types::*;
+
+#[derive(Copy, Clone)]
+/// Uniform buffer object, which contains uniform data stored in the GPU.
+pub struct UniformBuffer {
+    /// Unique ID associated to the object.
+    _id: u32,
+
+    /// Size of the data.
+    pub _size: u32,
+
+    /// Usage of the data.
+    pub _usage: GLenum,
+}
+
+impl UniformBuffer {
+    /// Generate a new uniform buffer.
+    pub fn new(size: u32) -> Self {
+        let mut ubo_id = 0;
+        unsafe {
+            gl::GenBuffers(1, &mut ubo_id);
+        }
+
+        return UniformBuffer {
+            _id: ubo_id,
+            _size: size,
+            _usage: gl::STATIC_DRAW,
+        };
+    }
+
+    /// Set the usage of the uniform buffer.
+    pub fn usage(mut self, new_usage: GLenum) -> Self {
+        self._usage = new_usage;
+        return self;
+    }
+
+    /// Buffer data to this uniform buffer.
+    pub fn buffer_data<T>(&self, data: &[T]) -> Self {
+        self.bind();
+        unsafe {
+            gl::BufferData(
+                gl::UNIFORM_BUFFER,
+                (data.len() * size_of::<T>()) as isize,
+                data.as_ptr() as *const GLvoid,
+                self._usage,
+            );
+        }
+        return *self;
+    }
+
+    /// Binds the uniform buffer to the given index in memory.
+    pub fn bind_index(&self, index: u32) {
+        unsafe {
+            gl::BindBufferBase(gl::UNIFORM_BUFFER, index, self._id);
+        }
+    }
+}
+
+impl Buffer for UniformBuffer {
+    fn bind(&self) {
+        unsafe {
+            gl::BindBuffer(gl::UNIFORM_BUFFER, self._id);
+        }
+    }
+    
+    fn unbind(&self) {
+        unsafe {
+            gl::BindBuffer(gl::UNIFORM_BUFFER, 0);
+        }
+    }
+
+    fn delete(&self) {
+        unsafe {
+            gl::DeleteBuffers(1, &self._id);
+        }
+    }
+}
