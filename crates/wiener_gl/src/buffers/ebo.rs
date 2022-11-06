4bf8f92c7b6b6bc61eea8426e0c170295c806074
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-06 17:48:58 -0300

    Implemented element buffers

---
+++ crates/wiener_gl/src/buffers/ebo.rs
@@ -0,0 +1,69 @@
+use super::buffer::Buffer;
+use std::mem::size_of;
+
+use gl;
+use gl::types::*;
+
+/// Element buffer object, which contains triangle data stored in the GPU.
+pub struct ElementBuffer {
+    /// Unique ID associated to the object.
+    _id: u32,
+
+    /// Usage of the data.
+    pub _usage: GLenum,
+}
+
+impl ElementBuffer {
+    /// Generate a new vertex buffer.
+    pub fn new() -> Self {
+        let mut ebo_id = 0;
+        unsafe {
+            gl::GenBuffers(1, &mut ebo_id);
+        }
+
+        return ElementBuffer {
+            _id: ebo_id,
+            _usage: gl::STATIC_DRAW,
+        };
+    }
+
+    /// Set the usage of the element buffer.
+    pub fn usage(mut self, new_usage: GLenum) -> Self {
+        self._usage = new_usage;
+        return self;
+    }
+
+    /// Buffer indices to this element buffer.
+    pub fn buffer_indices<T>(self, indices: &[T]) -> Self {
+        self.bind();
+        unsafe {
+            gl::BufferData(
+                gl::ELEMENT_ARRAY_BUFFER,
+                (indices.len() * size_of::<T>()) as isize,
+                indices.as_ptr() as *const GLvoid,
+                self._usage,
+            );
+        }
+        return self;
+    }
+}
+
+impl Buffer for ElementBuffer {
+    fn bind(&self) {
+        unsafe {
+            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, self._id);
+        }
+    }
+    
+    fn unbind(&self) {
+        unsafe {
+            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, 0);
+        }
+    }
+
+    fn delete(&self) {
+        unsafe {
+            gl::DeleteBuffers(1, &self._id);
+        }
+    }
+}
