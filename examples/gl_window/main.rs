89a2014850e7a2d0a255c25240bf2122505c245b
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-06 21:40:47 -0300

    Made the example better

--- examples/gl_window/main.rs
+++ examples/gl_window/main.rs
@@ -1,5 +1,6 @@
-use wiener::gl::GLWindow;
+use wiener::gl::{GLWindow, GLManager};
 use wiener::WindowDescriptor;
+use gl;
 
 fn main() {
     let window_descriptor = WindowDescriptor::builder()
@@ -7,7 +8,12 @@ fn main() {
         .title("Window example");
     let mut window = GLWindow::builder().descriptor(window_descriptor).build();
 
+    GLManager::clear_color(0.1, 0.1, 0.3, 1.0);
+
     while !window.should_close() {
         window.poll_events();
+        GLManager::clear(gl::COLOR_BUFFER_BIT);
+
+        window.swap_buffers();
     }
 }
