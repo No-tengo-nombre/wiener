592e8fb627053f109545623248930a53a570fcfa
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-07 19:07:17 -0300

    Made basic functions for a Mesh

--- crates/wiener_gl/src/shapes/mesh.rs
+++ crates/wiener_gl/src/shapes/mesh.rs
@@ -1,3 +1,36 @@
+use crate::{VertexArray, VertexBuffer, ElementBuffer, ShaderProgram, Texture2D, Buffer};
+
 pub struct Mesh {
-    
+    _vao: VertexArray,
+    _vbo: VertexBuffer,
+    _ebo: ElementBuffer,
+    _shader: ShaderProgram,
+    _textures: Vec<Texture2D>,
+}
+
+impl Mesh {
+    pub fn builder() -> Self {
+        return Mesh {
+            _vao: VertexArray::builder(),
+            _vbo: VertexBuffer::new(),
+            _ebo: ElementBuffer::new(),
+            _shader: ShaderProgram::new(),
+            _textures: [].to_vec(),
+        };
+    }
+
+    pub fn vertices<T>(self, new_vertices: &[T]) -> Self {
+        self._vbo.buffer_data(new_vertices);
+        return self;
+    }
+
+    pub fn indices<T>(self, new_indices: &[T]) -> Self {
+        self._ebo.buffer_data(new_indices);
+        return self;
+    }
+
+    pub fn vao(mut self, new_vao: VertexArray) -> Self {
+        self._vao = new_vao;
+        return self;
+    }
 }
