ec11698bfb2c3e2011db06fcf688c5ba6e51ae10
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-06 22:59:46 -0300

    Created files to start creating meshes

--- crates/wiener_gl/src/lib.rs
+++ crates/wiener_gl/src/lib.rs
@@ -4,10 +4,12 @@ pub mod prelude;
 
 mod buffers;
 mod gl_manager;
+mod shapes;
 mod textures;
 mod window;
 
 pub use buffers::*;
 pub use gl_manager::*;
+pub use shapes::*;
 pub use textures::*;
 pub use window::*;
