5a036c9f76fc4d7f091697872f27f780cabbeb01
Cristobal Allendes <callendes.molina@gmail.com>
2022-12-19 11:37:05 -0300

    Changed the examples to be consistent with the changes

--- examples/gl_window/main.rs
+++ examples/gl_window/main.rs
@@ -3,9 +3,12 @@ use wiener::WindowDescriptor;
 use gl;
 
 fn main() {
-    let window_descriptor = WindowDescriptor::builder()
-        .dimensions(1000, 1000)
-        .title("Window example");
+    let window_descriptor = WindowDescriptor {
+        width: 1000,
+        height: 1000,
+        title: "Window example".to_string(),
+        ..Default::default()
+    };
     let mut window = GLWindow::builder().descriptor(window_descriptor).build();
 
     GLManager::clear_color(0.1, 0.1, 0.3, 1.0);
