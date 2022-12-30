afa7d3beeaa3fa848b9974b78ddfc27e70189a2d
Cristobal Allendes <callendes.molina@gmail.com>
2022-12-30 19:39:46 -0300

    Added a function to set the polygon mode

--- crates/wiener_gl/src/gl_manager.rs
+++ crates/wiener_gl/src/gl_manager.rs
@@ -44,9 +44,16 @@ impl GLManager {
     }
 
     pub fn viewport(start_x: i32, start_y: i32, width: i32, height: i32) {
-        log::trace!("Setting viewport to {:?}x{:?} at ({:?}, {:?})", width, height, start_x, start_y);
+        log::trace!("GLManager :: Setting viewport to {:?}x{:?} at ({:?}, {:?})", width, height, start_x, start_y);
         unsafe {
             gl::Viewport(start_x, start_y, width, height);
+        }
+    }
+
+    pub fn polygon_mode(face: GLenum, mode: GLenum) {
+        log::info!("GLManager :: Setting polygon mode on face {:?} to {:?}", face, mode);
+        unsafe {
+            gl::PolygonMode(face, mode);
         }
     }
 }
