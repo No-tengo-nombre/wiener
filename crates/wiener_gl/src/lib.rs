ec38a2ef892442626b359e78ebf65176511f7900
Cristobal Allendes <callendes.molina@gmail.com>
2023-01-01 23:32:51 -0300

    Fixed a typo in the crate description, removed the functions from_off and from_obj in Mesh and replaced them with from_file

--- crates/wiener_gl/src/lib.rs
+++ crates/wiener_gl/src/lib.rs
@@ -1,8 +1,7 @@
-//! Create that contains the code to connect Wiener with OpenGL
-
-pub mod prelude;
+//! Crate that contains the code to connect Wiener with OpenGL
 
 mod buffers;
+mod file_handlers;
 mod framebuffer;
 mod gl_manager;
 mod shader;
@@ -12,6 +11,7 @@ mod types;
 mod window;
 
 pub use buffers::*;
+pub use file_handlers::*;
 pub use framebuffer::*;
 pub use gl_manager::*;
 pub use shader::*;
@@ -19,3 +19,5 @@ pub use shapes::*;
 pub use textures::*;
 pub use types::*;
 pub use window::*;
+
+pub mod prelude;
