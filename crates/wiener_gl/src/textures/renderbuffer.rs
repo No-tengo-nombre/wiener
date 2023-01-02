4971dbd9d0a1d5984ad76c3a9085987270670edb
Cristobal Allendes <callendes.molina@gmail.com>
2023-01-02 02:31:09 -0300

    Added logs

--- crates/wiener_gl/src/textures/renderbuffer.rs
+++ crates/wiener_gl/src/textures/renderbuffer.rs
@@ -25,11 +25,13 @@ impl RenderBuffer {
         unsafe {
             gl::GenRenderbuffers(1, &mut rbo);
         }
+        log::info!("RenderBuffer :: Creating renderbuffer {:?}", rbo);
         return RenderBuffer { _id: rbo };
     }
 
     /// Set up the render buffer, giving it a format, width and height.
     pub fn set_up(self, format: GLenum, width: i32, height: i32) -> Self {
+        log::debug!("RenderBuffer :: Setting up render buffer");
         self.bind();
         unsafe {
             gl::RenderbufferStorage(gl::RENDERBUFFER, format, width, height);
@@ -40,6 +42,7 @@ impl RenderBuffer {
 
     /// Set up a multisampled renderbuffer.
     pub fn set_up_multisample(self, samples: i32, format: GLenum, width: i32, height: i32) -> Self {
+        log::debug!("RenderBuffer :: Setting up multisampled render buffer");
         self.bind();
         unsafe {
             gl::RenderbufferStorageMultisample(gl::RENDERBUFFER, samples, format, width, height);
