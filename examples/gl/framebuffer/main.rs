a1bc4e41cc58bb99bf42487231b73935220e8e57
Cristobal Allendes <callendes.molina@gmail.com>
2023-01-02 01:44:30 -0300

    Made the framebuffer example use the spaceship model

--- examples/gl/framebuffer/main.rs
+++ examples/gl/framebuffer/main.rs
@@ -19,45 +19,42 @@ fn main() {
         })
         .build();
 
+    log::debug!("gl_framebuffer :: Enabling depth testing");
+    GLManager::enable(gl::DEPTH_TEST);
+
     log::debug!("gl_framebuffer :: Initializing framebuffer texture");
     let fbo_texture = Texture2D::default()
         .tex_num(0)
         .build()
         .buffer_empty(WINDOW_WIDTH, WINDOW_HEIGHT);
 
+    log::debug!("gl_framebuffer :: Initializing depth renderbuffer");
+    let fbo_depth = RenderBuffer::new()
+        .set_up(gl::DEPTH24_STENCIL8, WINDOW_WIDTH, WINDOW_HEIGHT);
+
     log::debug!("gl_framebuffer :: Initializing framebuffer");
-    let fbo = FrameBuffer::new().attach_texture2d(0, &fbo_texture);
+    let fbo = FrameBuffer::new()
+        .attach_texture2d(0, &fbo_texture)
+        .attach_renderbuffer(gl::DEPTH_STENCIL_ATTACHMENT, &fbo_depth);
     fbo.bind();
     fbo.verify();
 
-    log::debug!("gl_framebuffer :: Making triangle shader");
-    let triangle_shader_arr = [
-        Shader::from_file("examples/gl/framebuffer/resources/triangle.vert"),
-        Shader::from_file("examples/gl/framebuffer/resources/triangle.frag"),
-    ];
-    let triangle_shader = ShaderProgram::from_array(&triangle_shader_arr);
-
     log::debug!("gl_framebuffer :: Making framebuffer shader");
     let framebuffer_shader_arr = [
-        Shader::from_file("examples/gl/framebuffer/resources/framebuffer.vert"),
-        Shader::from_file("examples/gl/framebuffer/resources/framebuffer.frag"),
+        Shader::from_file("examples/gl/framebuffer/resources/shaders/framebuffer.vert"),
+        Shader::from_file("examples/gl/framebuffer/resources/shaders/framebuffer.frag"),
     ];
     let framebuffer_shader = ShaderProgram::from_array(&framebuffer_shader_arr);
     framebuffer_shader.uniform_1f("u_screen_x", WINDOW_WIDTH as f32);
     framebuffer_shader.uniform_1f("u_screen_y", WINDOW_HEIGHT as f32);
 
-    let triangle_layout = [
-        VertexAttribute {
-            location: 0,
-            size: 3,
-            data_type: gl::FLOAT,
-        },
-        VertexAttribute {
-            location: 1,
-            size: 3,
-            data_type: gl::FLOAT,
-        },
+    log::debug!("gl_framebuffer :: Making ship shader");
+    let ship_shader_arr = [
+        Shader::from_file("examples/gl/framebuffer/resources/shaders/ship.vert"),
+        Shader::from_file("examples/gl/framebuffer/resources/shaders/ship.frag"),
     ];
+    let ship_shader = ShaderProgram::from_array(&ship_shader_arr);
+
     let screen_quad_layout = [
         VertexAttribute {
             location: 0,
@@ -71,22 +68,25 @@ fn main() {
         },
     ];
 
-    log::debug!("gl_framebuffer :: Making triangle mesh");
-    let mut triangle_rotation = Mesh::<f32, u32>::new(&triangle_shader)
-        .vertices(&[
-            -0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0,
-            1.0_f32,
-        ])
-        .indices(&[0, 1, 2])
-        .layout(&triangle_layout);
+    let vertex_layout = [
+        VertexAttribute {
+            location: 0,
+            size: 3,
+            data_type: gl::FLOAT,
+        },
+        VertexAttribute {
+            location: 1,
+            size: 3,
+            data_type: gl::FLOAT,
+        },
+    ];
 
-    let mut triangle_translation = Mesh::<f32, u32>::new(&triangle_shader)
-        .vertices(&[
-            -0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0,
-            1.0_f32,
-        ])
-        .indices(&[0, 1, 2])
-        .layout(&triangle_layout);
+    log::debug!("gl_framebuffer :: Loading ship mesh");
+    let mut ship = Mesh::<f32, u32>::from_file(
+        "examples/gl/framebuffer/resources/models/XJ5 X-wing starfighter.obj",
+        &ship_shader,
+    )
+    .layout(&vertex_layout);
 
     log::debug!("gl_framebuffer :: Making screen quad");
     let screen_quad_textures = [fbo_texture];
@@ -102,8 +102,7 @@ fn main() {
     log::debug!("gl_framebuffer :: Setting clear color");
     GLManager::clear_color(0.1, 0.1, 0.3, 1.0);
 
-    const ROTATION_SPEED: f32 = 10.0;
-    const TRANSLATION_SPEED: f32 = 2.0;
+    const ROTATION_SPEED: f32 = 1.0;
 
     log::debug!("gl_framebuffer :: Starting the render loop");
     let mut viewport;
@@ -114,25 +113,24 @@ fn main() {
 
         // Set the time
         window_time = window.get_time();
-        triangle_shader.uniform_1f("u_time", window_time);
-
         viewport = window.get_window().get_framebuffer_size();
+        ship_shader.uniform_1f("u_time", window_time);
 
-        GLManager::clear(gl::COLOR_BUFFER_BIT);
+        GLManager::clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+        GLManager::enable(gl::DEPTH_TEST);
         GLManager::viewport(0, 0, viewport.0, viewport.1);
 
-        triangle_rotation.model_mat =
-            math::rotation(0.0, 0.0, (ROTATION_SPEED * window_time).sin());
-        triangle_translation.model_mat =
-            math::translation((TRANSLATION_SPEED * window_time).sin(), 0.0, 0.0);
-
-        triangle_rotation.draw();
-        triangle_translation.draw();
+        ship.model_mat = math::matmul(
+            math::rotation(ROTATION_SPEED * window_time, 0.0, 0.0),
+            math::scaling(0.5, 0.5, 0.5),
+        );
+        ship.draw();
 
         fbo.unbind();
 
         // === Render framebuffer texture to a quad === //
         GLManager::clear(gl::COLOR_BUFFER_BIT);
+        GLManager::disable(gl::DEPTH_TEST);
         GLManager::viewport(0, 0, viewport.0, viewport.1);
 
         screen_quad.draw();
--- examples/gl/framebuffer/resources/shaders/ship.vert
+++ examples/gl/framebuffer/resources/shaders/ship.vert
@@ -1,7 +1,7 @@
 #version 460 core
 
 layout (location = 0) in vec3 in_position;
-layout (location = 1) in vec3 in_color;
+layout (location = 1) in vec3 in_normal;
 
 out vec3 color;
 
@@ -11,5 +11,5 @@ uniform mat4 u_projection;
 
 void main() {
     gl_Position = u_projection * u_view * u_model * vec4(in_position, 1.0f);
-    color = in_color;
+    color = vec3(0.0, 0.0, 0.5 * (in_position.y + 1.0));
 }
