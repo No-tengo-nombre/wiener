aee20366f628b3f70198b6f3ab87e77b5db2c6ea
Cristobal Allendes <callendes.molina@gmail.com>
2023-01-04 21:39:51 -0300

    Added functions to save different formats and different data types

--- crates/wiener_utils/src/image.rs
+++ crates/wiener_utils/src/image.rs
@@ -12,9 +12,49 @@ pub fn load(path: &str) -> (image::DynamicImage, i32, i32) {
     return (img_data, width, height);
 }
 
-pub fn save_image(path: &str, buffer: &[u8], width: i32, height: i32) {
+pub fn save_image_rgb_f32(path: &str, buffer: &[f32], width: i32, height: i32) {
     log::info!(
-        "save_image :: Saving {:?}x{:?} image to file {:?}",
+        "save_image :: Saving {:?}x{:?} f32 RGB image to file {:?}",
+        width,
+        height,
+        path
+    );
+
+    log::debug!("save_image :: Loading image from data");
+    let result =
+        image::ImageBuffer::<image::Rgb<f32>, &[f32]>::from_raw(width as u32, height as u32, buffer)
+            .expect("Not enough memory was allocated");
+
+    log::debug!("save_image :: Saving image to file");
+    match result.save(path) {
+        Ok(_) => {}
+        _ => log::warn!("save_image :: Could not save image"),
+    }
+}
+
+pub fn save_image_rgba_f32(path: &str, buffer: &[f32], width: i32, height: i32) {
+    log::info!(
+        "save_image :: Saving {:?}x{:?} f32 RGB image to file {:?}",
+        width,
+        height,
+        path
+    );
+
+    log::debug!("save_image :: Loading image from data");
+    let result =
+        image::ImageBuffer::<image::Rgb<f32>, &[f32]>::from_raw(width as u32, height as u32, buffer)
+            .expect("Not enough memory was allocated");
+
+    log::debug!("save_image :: Saving image to file");
+    match result.save(path) {
+        Ok(_) => {}
+        _ => log::warn!("save_image :: Could not save image"),
+    }
+}
+
+pub fn save_image_rgb_u8(path: &str, buffer: &[u8], width: i32, height: i32) {
+    log::info!(
+        "save_image :: Saving {:?}x{:?} u8 RGB image to file {:?}",
         width,
         height,
         path
@@ -23,6 +63,66 @@ pub fn save_image(path: &str, buffer: &[u8], width: i32, height: i32) {
     log::debug!("save_image :: Loading image from data");
     let result =
         image::ImageBuffer::<image::Rgb<u8>, &[u8]>::from_raw(width as u32, height as u32, buffer)
+            .expect("Not enough memory was allocated");
+
+    log::debug!("save_image :: Saving image to file");
+    match result.save(path) {
+        Ok(_) => {}
+        _ => log::warn!("save_image :: Could not save image"),
+    }
+}
+
+pub fn save_image_rgba_u8(path: &str, buffer: &[u8], width: i32, height: i32) {
+    log::info!(
+        "save_image :: Saving {:?}x{:?} u8 RGB image to file {:?}",
+        width,
+        height,
+        path
+    );
+
+    log::debug!("save_image :: Loading image from data");
+    let result =
+        image::ImageBuffer::<image::Rgb<u8>, &[u8]>::from_raw(width as u32, height as u32, buffer)
+            .expect("Not enough memory was allocated");
+
+    log::debug!("save_image :: Saving image to file");
+    match result.save(path) {
+        Ok(_) => {}
+        _ => log::warn!("save_image :: Could not save image"),
+    }
+}
+
+pub fn save_image_rgb_u16(path: &str, buffer: &[u16], width: i32, height: i32) {
+    log::info!(
+        "save_image :: Saving {:?}x{:?} u16 RGB image to file {:?}",
+        width,
+        height,
+        path
+    );
+
+    log::debug!("save_image :: Loading image from data");
+    let result =
+        image::ImageBuffer::<image::Rgb<u16>, &[u16]>::from_raw(width as u32, height as u32, buffer)
+            .expect("Not enough memory was allocated");
+
+    log::debug!("save_image :: Saving image to file");
+    match result.save(path) {
+        Ok(_) => {}
+        _ => log::warn!("save_image :: Could not save image"),
+    }
+}
+
+pub fn save_image_rgba_u16(path: &str, buffer: &[u16], width: i32, height: i32) {
+    log::info!(
+        "save_image :: Saving {:?}x{:?} u16 RGB image to file {:?}",
+        width,
+        height,
+        path
+    );
+
+    log::debug!("save_image :: Loading image from data");
+    let result =
+        image::ImageBuffer::<image::Rgb<u16>, &[u16]>::from_raw(width as u32, height as u32, buffer)
             .expect("Not enough memory was allocated");
 
     log::debug!("save_image :: Saving image to file");
