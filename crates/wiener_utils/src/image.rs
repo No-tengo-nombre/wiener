598600211f145404069e6778ea8e0cfebac9904b
Cristobal Allendes <callendes.molina@gmail.com>
2023-01-07 21:36:39 -0300

    Made the errors be more explicit

--- crates/wiener_utils/src/image.rs
+++ crates/wiener_utils/src/image.rs
@@ -28,7 +28,7 @@ pub fn save_image_rgb_f32(path: &str, buffer: &[f32], width: i32, height: i32) {
     log::debug!("save_image :: Saving image to file");
     match result.save(path) {
         Ok(_) => {}
-        _ => log::warn!("save_image :: Could not save image"),
+        Err(r) => log::warn!("save_image :: Could not save image, found error '{r}'"),
     }
 }
 
@@ -42,13 +42,13 @@ pub fn save_image_rgba_f32(path: &str, buffer: &[f32], width: i32, height: i32) 
 
     log::debug!("save_image :: Loading image from data");
     let result =
-        image::ImageBuffer::<image::Rgb<f32>, &[f32]>::from_raw(width as u32, height as u32, buffer)
+        image::ImageBuffer::<image::Rgba<f32>, &[f32]>::from_raw(width as u32, height as u32, buffer)
             .expect("Not enough memory was allocated");
 
     log::debug!("save_image :: Saving image to file");
     match result.save(path) {
         Ok(_) => {}
-        _ => log::warn!("save_image :: Could not save image"),
+        Err(r) => log::warn!("save_image :: Could not save image, found error '{r}'"),
     }
 }
 
@@ -67,8 +67,8 @@ pub fn save_image_rgb_u8(path: &str, buffer: &[u8], width: i32, height: i32) {
 
     log::debug!("save_image :: Saving image to file");
     match result.save(path) {
-        Ok(_) => {}
-        _ => log::warn!("save_image :: Could not save image"),
+        Ok(_) => {},
+        Err(r) => log::warn!("save_image :: Could not save image, found error '{r}'"),
     }
 }
 
@@ -82,13 +82,13 @@ pub fn save_image_rgba_u8(path: &str, buffer: &[u8], width: i32, height: i32) {
 
     log::debug!("save_image :: Loading image from data");
     let result =
-        image::ImageBuffer::<image::Rgb<u8>, &[u8]>::from_raw(width as u32, height as u32, buffer)
+        image::ImageBuffer::<image::Rgba<u8>, &[u8]>::from_raw(width as u32, height as u32, buffer)
             .expect("Not enough memory was allocated");
 
     log::debug!("save_image :: Saving image to file");
     match result.save(path) {
         Ok(_) => {}
-        _ => log::warn!("save_image :: Could not save image"),
+        Err(r) => log::warn!("save_image :: Could not save image, found error '{r}'"),
     }
 }
 
@@ -108,7 +108,7 @@ pub fn save_image_rgb_u16(path: &str, buffer: &[u16], width: i32, height: i32) {
     log::debug!("save_image :: Saving image to file");
     match result.save(path) {
         Ok(_) => {}
-        _ => log::warn!("save_image :: Could not save image"),
+        Err(r) => log::warn!("save_image :: Could not save image, found error '{r}'"),
     }
 }
 
@@ -122,12 +122,12 @@ pub fn save_image_rgba_u16(path: &str, buffer: &[u16], width: i32, height: i32) 
 
     log::debug!("save_image :: Loading image from data");
     let result =
-        image::ImageBuffer::<image::Rgb<u16>, &[u16]>::from_raw(width as u32, height as u32, buffer)
+        image::ImageBuffer::<image::Rgba<u16>, &[u16]>::from_raw(width as u32, height as u32, buffer)
             .expect("Not enough memory was allocated");
 
     log::debug!("save_image :: Saving image to file");
     match result.save(path) {
         Ok(_) => {}
-        _ => log::warn!("save_image :: Could not save image"),
+        Err(r) => log::warn!("save_image :: Could not save image, found error '{r}'"),
     }
 }
